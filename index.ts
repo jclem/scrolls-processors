94803eae934e32d478e37ebb842dc3a0ae1b8716
Jonathan Clem <j@jclem.me>
2024-01-15 11:31:14 -0500

    Log file content

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-console.log("Hello via Bun!");
+import { assert } from "@jclem/assert";
+
+const path = assert(Bun.argv.at(2))
+const file = Bun.file(path)
+const content = await file.json()
+console.log(content);
